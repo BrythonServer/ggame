4d319bade1a1e065706ebd708e2ed57167717a16
Eric Dennison <ericd@netdenizen.com>
2015-07-27 13:20:14 -0400

    Updated from Brython Server: 7/27/2015 1:20:13 PM

--- ggame.py
+++ ggame.py
@@ -1,7 +1,16 @@
+from browser import window, document
+from random import randint
+from javascript import JSObject, JSConstructor
+
+# depends on pixi.js 
+PIXI = JSObject(window.PIXI)
+PIXI_ImageLoader = JSConstructor(PIXI.ImageLoader)
+
 
 class ImageAsset(object):
     def __init__(self, url):
-        
+        self.url = url
+        PIXI_ImageLoader(url, False)
 
 
 if __name__ == '__main__':
