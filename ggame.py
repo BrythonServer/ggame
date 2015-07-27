1bc15853986cc9bb757a3349096a7c06bcfad2b1
Eric Dennison <ericd@netdenizen.com>
2015-07-27 13:14:23 -0400

    Updated from Brython Server: 7/27/2015 1:14:22 PM

--- ggame.py
+++ ggame.py
@@ -1,3 +1,12 @@
 
+class ImageAsset(object):
+    def __init__(self, url):
+        
+
+
 if __name__ == '__main__':
-  print("ggame test.")
+    bunnyurl = "https://github.com/tiggerntatie/brython-\
+server-testing/blob/master/bunny.png?raw=true"
+    print("ggame test.")
+    asset = ImageAsset(bunnyurl)
+    
