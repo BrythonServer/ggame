d33692167c386c4abac6a9226a03b0d7b1c264d0
Eric Dennison <ericd@netdenizen.com>
2015-07-27 14:29:37 -0400

    Updated from Brython Server: 7/27/2015 2:29:36 PM

--- ggame.py
+++ ggame.py
@@ -2,10 +2,15 @@ from browser import window, document
 from random import randint
 from javascript import JSObject, JSConstructor
 
+w = window.open("", "")
+
+
 # depends on pixi.js 
 PIXI = JSObject(window.PIXI)
 PIXI_loader = JSObject(PIXI.loader)
 PIXI_loader.reset()
+
+Sprite = JSConstructor(PIXI.Sprite)
 
 def loadComplete(ev):
     print("loaded asset!")
@@ -15,14 +20,20 @@ class ImageAsset(object):
         self.name = name
         self.url = url
         PIXI_loader.add(name, url)
-        PIXI_loader.on('progress', self.loadComplete)
+        PIXI_loader.on('complete', self.loadComplete)
         PIXI_loader.load()
 
     def loadComplete(self, ev):
         print("loaded asset!")
 
+
+
 if __name__ == '__main__':
     bunnyurl = "bunny.png"
     print("ggame test.")
     asset = ImageAsset('bunny', bunnyurl)
-    
+    STAGE = Stage()
+    RENDERER = PIXI.autoDetectRenderer(1000,650, {'transparent':True})
+    stage.addChild(Sprite.fromImage(bunnyurl)
+    w.document.body.appendChild(RENDERER.view)
+    RENDERER.render(STAGE)    
