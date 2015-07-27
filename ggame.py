b8fa55d62cd97702e1682d3383f82f52f7f31745
Eric Dennison <ericd@netdenizen.com>
2015-07-27 16:25:31 -0400

    Updated from Brython Server: 7/27/2015 4:25:30 PM

--- ggame.py
+++ ggame.py
@@ -10,31 +10,37 @@ PIXI = JSObject(window.PIXI)
 PIXI_loader = JSObject(PIXI.loader)
 PIXI_loader.reset()
 
-Sprite = JSConstructor(PIXI.Sprite)
+PIXI_Sprite = JSConstructor(PIXI.Sprite)
 PIXI_Texture = JSConstructor(PIXI.Texture)
 PIXI_Rectangle = JSConstructor(PIXI.Rectangle)
 
+
+class Rectangle(object):
+    def __init__(self, x, y, w, h):
+        self.x = x
+        self.y = y
+        self.w = w
+        self.h = h
+        self.PIXI = PIXI_Rectangle(x,y,w,h)
+    
+    @property
+    def center(self):
+        return (self.x + self.w//2, self.y + self.h//2)
+    
+    @center.setter
+    def center(self, value):
+        c = self.center
+        self.x += value[0] - c[0]
+        self.y += value[1] - c[1]
+        
 class ImageAsset(object):
     def __init__(self, url):
-        window.brython_loaded = self.imageLoaded
-        self.isloaded = False
-        self.texture = None
         self.url = url
-        PIXI_loader.add(url)
-        PIXI_loader.once('complete',window.brython_loaded);
-        PIXI_loader.load();
+        self.texture = PIXI.Texture.fromImage(url, False)
 
-    def imageLoaded(self):
-        self.texture = PIXI.Texture.fromImage(self.url, False)
-        print("loaded..")
-
-
-#class ImageAsset(object):
-#    def __init__(self, url):
-#        self.url = url
-#        self.texture = PIXI.Texture.fromImage(url, False)
-
-
+class Sprite(object):
+    def __init__(self, texture, frame):
+        
 
 if __name__ == '__main__':
     def animate(fake):
@@ -47,11 +53,9 @@ if __name__ == '__main__':
     Stage = JSConstructor(PIXI.Container)
     STAGE = Stage()
     RENDERER = PIXI.autoDetectRenderer(1000,650, {'transparent':True})
-    s = Sprite(bunny.texture)
     frame = PIXI_Rectangle(0,0,30,30)
 
-    # bunny.imageLoaded()
-    s1 = Sprite(PIXI_Texture(bunny.texture, frame))
+    s1 = PIXI_Sprite(PIXI_Texture(bunny.texture, frame))
     STAGE.addChild(s1)
     w.document.body.appendChild(RENDERER.view)
     w.requestAnimationFrame(animate)
