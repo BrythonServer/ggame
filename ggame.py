91b267063a59e22f32f504c57d4b0fa9319aba55
Eric Dennison <ericd@netdenizen.com>
2015-07-27 13:27:56 -0400

    Updated from Brython Server: 7/27/2015 1:27:55 PM

--- ggame.py
+++ ggame.py
@@ -4,18 +4,19 @@ from javascript import JSObject, JSConstructor
 
 # depends on pixi.js 
 PIXI = JSObject(window.PIXI)
-PIXI_ImageLoader = JSConstructor(PIXI.ImageLoader)
+PIXI_loader = JSObject(PIXI.loader)
 
 
 class ImageAsset(object):
-    def __init__(self, url):
+    def __init__(self, name, url):
+        self.name = name
         self.url = url
-        PIXI_ImageLoader(url, False)
+        PIXI_loader.add(name, url)
 
 
 if __name__ == '__main__':
     bunnyurl = "https://github.com/tiggerntatie/brython-\
 server-testing/blob/master/bunny.png?raw=true"
     print("ggame test.")
-    asset = ImageAsset(bunnyurl)
+    asset = ImageAsset('bunny', bunnyurl)
     
