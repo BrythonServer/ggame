5befb4d118091a711a47652af13e4e33a6ce5ac0
Eric Dennison <ericd@netdenizen.com>
2015-07-30 17:18:59 -0400

    Updated from Brython Server: 7/30/2015 5:18:58 PM

--- ggame.py
+++ ggame.py
@@ -1,3 +1,9 @@
+from browser import window, document
+from javascript import JSObject, JSConstructor
+
+GFX = JSObject(window.PIXI)
+GFX_Rectangle = JSConstructor(GFX.Rectangle)
+GFX_Texture = JSObject(GFX.Texture)
 GFX_Texture_fromImage = JSConstructor(GFX_Texture.fromImage)
 GFX_Sprite = JSConstructor(GFX.Sprite)
 GFX_Graphics = JSConstructor(GFX.Graphics)()
