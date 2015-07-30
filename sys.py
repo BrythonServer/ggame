3021fc69fc7fe45f2d93d1ee71115313277af53c
Eric Dennison <ericd@netdenizen.com>
2015-07-30 16:30:32 -0400

    Update sys.py

--- sys.py
+++ sys.py
@@ -8,7 +8,29 @@ GFX_Texture_fromImage = JSConstructor(GFX_Texture.fromImage)
 GFX_Sprite = JSConstructor(GFX.Sprite)
 GFX_Graphics = JSConstructor(GFX.Graphics)()
 GFX_Text = JSConstructor(GFX.Text)
-GFX_Stage = JSConstructor(GFX.Container)()
 GFX_DetectRenderer = GFX.autoDetectRenderer 
 SND = JSObject(window.buzz)
 SND_Sound = JSConstructor(SND.sound)
+
+class GFX_Window(object):
+  
+  def __init__(self, width, height, onclose):
+    self._w = window.open("", "")
+    self._stage = JSConstructor(GFX.Container)()
+    self._renderer = GFX.autoDetectRenderer(width, height, {'transparent':True})
+    self._w.document.body.appendChild(self.renderer.view)
+    self._w.onunload = onclose
+
+  def bind(self, evtspec, callback):
+    self._w.document.body.bind(evtspec, callback)
+    
+  def add(self, obj):
+    self._stage.addChild(obj)
+    
+  def remove(self, obj):
+    self._stage.removeChild(obj)
+    
+  def animate(self, stepcallback):
+    self._renderer.render(self._stage)
+    self._w.requestAnimationFrame(stepcallback)
+    
