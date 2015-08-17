f5c871d388f41e2fcebeba94f40d5853b53dae2a
Eric Dennison <ericd@netdenizen.com>
2015-08-17 17:51:21 -0400

    debugging

--- deploy.sh
+++ deploy.sh
@@ -13,13 +13,13 @@ cd out
 git init
 
 # inside this git repo we'll pretend to be a new user
-git config user.name "Travis CI"
-git config user.email "ericd@netdenizen.com"
+#git config user.name "Travis CI"
+#git config user.email "ericd@netdenizen.com"
 
 # The first and only commit to this new Git repo contains all the
 # files present with the commit message "Deploy to GitHub Pages".
-git add .
-git commit -m "Deploy to GitHub Pages"
+#git add .
+#git commit -m "Deploy to GitHub Pages"
 
 # Force push from the current repo's master branch to the remote
 # repo's gh-pages branch. (All previous history on the gh-pages branch
