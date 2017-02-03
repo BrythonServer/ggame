591dfc559fec1c4c5c319459d56658c4dbd6db0d
Eric Dennison <ericd@netdenizen.com>
2017-02-03 13:15:03 -0500

    Updated from Brython Server: 2/3/2017 1:15:03 PM

--- ggmath.py
+++ ggmath.py
@@ -8,10 +8,42 @@ la = LineAsset(100, 100, ls)
 
 class LineSegment(Sprite):
     def __init__(self, start, end, style=LineStyle(1, Color(0,1))):
+        self._start = start
+        self._end = end
         self.la = LineAsset(end[0]-start[0], end[1]-start[1], style)
         super().__init__(self.la, start)
         
+    @property
+    def start(self):
+        return self._start
+
+    @start.setter
+    def start(self, val):
+        self._start = val
+        self.GFX.points[0] = self._start[0]
+        self.GFX.points[1] = self._start[1]
+        
+    @property
+    def end(self):
+        return self._end
+
+    @end.setter
+    def end(self, val):
+        self._start = val
+        self.GFX.points[2] = self._end[0]
+        self.GFX.points[3 ] = self._end[1]
+        
+    def step(self):
+        self.start = (self.start[0]+1, self.start[1])
+        
+        
 l = LineSegment((100,100), (200,200))
+
+class MathApp(App):
+    
+    def step(self):
+        for spr in MathApp.getSpritesbyClass(LineSegment):
+            spr.step()
 
 ap = App()
 ap.run()
