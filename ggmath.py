7784bbffdd417a5834628cc86a87871aade64272
Eric Dennison <ericd@netdenizen.com>
2017-02-03 13:43:28 -0500

    Updated from Brython Server: 2/3/2017 1:43:28 PM

--- ggmath.py
+++ ggmath.py
@@ -7,11 +7,22 @@ la = LineAsset(100, 100, ls)
 #sp = Sprite(la, (100,100))
 
 class LineSegment(Sprite):
+    ls = LineStyle(1, Color(0x000000, 1))
+    la = LineAsset(0,0, LineSegment.ls)
+    
     def __init__(self, start, end, style=LineStyle(1, Color(0,1))):
         self._start = start
         self._end = end
-        self.la = LineAsset(end[0]-start[0], end[1]-start[1], style)
-        super().__init__(self.la, start)
+        self._style = style
+        super().__init__(LineSegment.la, start)
+        self._newSegment(self._start, self._end, self._style)
+        
+    def _newSegment(self, start, end, style):
+        self.GFX.destroy()
+        self.asset = LineAsset(end[0]-start[0], end[1]-start[1], style)
+        self.GFX = asset.GFX
+        self.GFX.visible = True        
+    
         
     @property
     def start(self):
@@ -20,8 +31,8 @@ class LineSegment(Sprite):
     @start.setter
     def start(self, val):
         self._start = val
-        self.GFX.currentPath.points[0] = self._start[0]
-        self.GFX.currentPath.points[1] = self._start[1]
+        self._newSegment(self._start, self._end, self._style)
+
         
     @property
     def end(self):
@@ -30,8 +41,7 @@ class LineSegment(Sprite):
     @end.setter
     def end(self, val):
         self._end = val
-        self.GFX.currentPath.points[2] = self._end[0]
-        self.GFX.currentPath.points[3] = self._end[1]
+        self.la = LineAsset(end[0]-start[0], end[1]-start[1], style)
         
     def step(self):
         self.start = (self.start[0]+1, self.start[1])
