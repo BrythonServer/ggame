90ef01ca380cb53ef1dbbe1c756d6e78b9585465
Eric Dennison <ericd@netdenizen.com>
2017-02-03 13:48:46 -0500

    Updated from Brython Server: 2/3/2017 1:48:46 PM

--- ggmath.py
+++ ggmath.py
@@ -2,20 +2,17 @@
 
 from ggame import Color, LineStyle, LineAsset, Sprite, App
 
-ls = LineStyle(3, Color(0x000000, 1))
-la = LineAsset(100, 100, ls)
-#sp = Sprite(la, (100,100))
 
 class LineSegment(Sprite):
-    ls = LineStyle(1, Color(0x000000, 1))
-    la = LineAsset(0,0, LineSegment.ls)
+    
+    la = LineAsset(0,0, LineStyle(1, Color(0x000000, 1)))
     
     def __init__(self, start, end, style=LineStyle(1, Color(0,1))):
         self._start = start
         self._end = end
         self._style = style
-        super().__init__(LineSegment.la, start)
-        self._newSegment(self._start, self._end, self._style)
+        #super().__init__(LineSegment.la, start)
+        #self._newSegment(self._start, self._end, self._style)
         
     def _newSegment(self, start, end, style):
         self.GFX.destroy()
@@ -23,7 +20,6 @@ class LineSegment(Sprite):
         self.GFX = self.asset.GFX
         self.GFX.visible = True        
     
-        
     @property
     def start(self):
         return self._start
@@ -45,16 +41,13 @@ class LineSegment(Sprite):
         
     def step(self):
         self.start = (self.start[0]+1, self.start[1])
-        
-        
+
 l = LineSegment((100,100), (200,200))
 
 class MathApp(App):
-    
     def step(self):
         for spr in MathApp.getSpritesbyClass(LineSegment):
-            pass
-            #spr.step()
+            spr.step()]
 
 ap = MathApp()
 ap.run()
