5696bbf3c3797168cfafc01db9bdfd4e460207d2
Eric Dennison <ericd@netdenizen.com>
2017-02-03 13:00:14 -0500

    Updated from Brython Server: 2/3/2017 1:00:13 PM

--- ggmath.py
+++ ggmath.py
@@ -4,7 +4,14 @@ from ggame import Color, LineStyle, LineAsset, Sprite, App
 
 ls = LineStyle(3, Color(0x000000, 1))
 la = LineAsset(100, 100, ls)
-sp = Sprite(la, (100,100))
+#sp = Sprite(la, (100,100))
+
+class LineSegment(Sprite):
+    def __init__(self, start, end, style=LineStyle(1, Color(0,1))):
+        self.la = LineAsset(end[0]-start[0], end[1]-start[1], style)
+        super().__init__(self.la, start)
+        
+l = LineSegment((100,100), (200,200))
 
 ap = App()
 ap.run()
