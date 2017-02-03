d1f6832cba54afe1f285df317638e9c4c11f1b4b
Eric Dennison <ericd@netdenizen.com>
2017-02-03 09:38:12 -0500

    Updated from Brython Server: 2/3/2017 9:38:12 AM

--- ggmath.py
+++ ggmath.py
@@ -2,7 +2,7 @@
 
 from ggame import Color, LineStyle, LineAsset, Sprite, App
 
-ls = LineStyle(3, Color(0xffffff, 1))
+ls = LineStyle(3, Color(0x000000, 1))
 la = LineAsset(100, 100, ls)
 sp = Sprite(la, (100,100))
 
