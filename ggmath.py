d8c317d4028f9efab37b794946cfa9ac98d7cbd5
Eric Dennison <ericd@netdenizen.com>
2017-02-03 09:37:41 -0500

    Updated from Brython Server: 2/3/2017 9:37:41 AM

--- ggmath.py
+++ ggmath.py
@@ -1,1 +1,10 @@
 # ggmath - ggame extensions for geometry and mathematics in the browser
+
+from ggame import Color, LineStyle, LineAsset, Sprite, App
+
+ls = LineStyle(3, Color(0xffffff))
+la = LineAsset(100, 100, ls)
+sp = Sprite(la, (100,100))
+
+ap = App()
+ap.run()
