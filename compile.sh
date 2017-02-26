f2b8464183ea9b3bb671189bbca081866d63773e
Eric Dennison <ericd@netdenizen.com>
2017-02-25 19:05:53 -0500

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -1,3 +1,5 @@
 export PYTHONPATH=./
 pdoc ggame.py --html --html-dir out --overwrite
+pdoc ggmath.py --html --html-dir out --overwrite
 mv out/ggame.m.html out/index.html
+mv out/ggmath.m.html out/ggmath.html
