686b470967d76d9e21b1c075f8d4382d036f511b
Eric Dennison <ericd@netdenizen.com>
2018-02-22 12:45:59 -0500

    Updated from Brython Server: 2/22/2018 12:45:59 PM

--- ggmath.py
+++ ggmath.py
@@ -590,7 +590,7 @@ class Circle(_MathVisual):
             return CircleAsset(pradius, style, fill)
         else:
             poly = self._buildPolygon(pcenter, pradius)
-            print(poly)
+            #print(poly)
             return PolygonAsset(poly, style, fill)
 
     def _buildPolygon(self, pcenter, pradius):
@@ -614,7 +614,6 @@ class Circle(_MathVisual):
         if disc <= 0:  # less than two solutions
             return []
         sdisc = sqrt(disc)
-        print(c)
         x = [(D*dy + dx*sdisc)/dr2 + c[0],  (D*dy - dx*sdisc)/dr2 + c[0]]
         y = [(-D*dx - dy*sdisc)/dr2 + c[1], (-D*dx + dy*sdisc)/dr2 + c[1]]
         getcoords = lambda x, y, c: [(x-c[0],y-c[1])] if x>=0 and x<=MathApp.width and y>=0 and y<=MathApp.width else []
