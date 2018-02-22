9d1707fe10ee9e160bc886fe31b80cfbf9ab8b55
Eric Dennison <ericd@netdenizen.com>
2018-02-22 12:36:17 -0500

    Updated from Brython Server: 2/22/2018 12:36:17 PM

--- ggmath.py
+++ ggmath.py
@@ -615,11 +615,11 @@ class Circle(_MathVisual):
             return []
         sdisc = sqrt(disc)
         print(c)
-        x = [(D*dy + dx*sdisc)/dr2 ,  (D*dy - dx*sdisc)/dr2 ]
-        y = [(-D*dx - dy*sdisc)/dr2 , (-D*dx + dy*sdisc)/dr2 ]
-        getcoords = lambda x, y: [(x,y)] if x>=0 and x<=MathApp.width and y>=0 and y<=MathApp.width else []
-        res = getcoords(x[0], y[0])
-        return res + getcoords(x[1], y[1])
+        x = [(D*dy + dx*sdisc)/dr2 + c[0],  (D*dy - dx*sdisc)/dr2 + c[0]]
+        y = [(-D*dx - dy*sdisc)/dr2 + c[1], (-D*dx + dy*sdisc)/dr2 + c[1]]
+        getcoords = lambda x, y, c: [(x-c[0],y-c[1])] if x>=0 and x<=MathApp.width and y>=0 and y<=MathApp.width else []
+        res = getcoords(x[0], y[0], c)
+        return res + getcoords(x[1], y[1], c)
 
 
 
