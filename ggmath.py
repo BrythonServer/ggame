6f45f5acdbd671f2d262d46da0cbc5f283f6dde7
Eric Dennison <ericd@netdenizen.com>
2018-02-22 20:29:16 -0500

    Updated from Brython Server: 2/22/2018 8:29:15 PM

--- ggmath.py
+++ ggmath.py
@@ -634,6 +634,7 @@ class Circle(_MathVisual):
         cw = 0
         for p in range(3):
             cw = cw + (plist[p+1][0]-plist[p][0])*(plist[p+1][1]+plist[p][1])
+        print("raw cw ", cw)
         cw = self._sgn(cw)
         cw = 1 if cw > 0 else 0
         print("cw ",cw)
