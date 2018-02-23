122e6e961e9a5c4c17e397cdb719de98f20ac725
Eric Dennison <ericd@netdenizen.com>
2018-02-22 22:27:39 -0500

    Updated from Brython Server: 2/22/2018 10:27:38 PM

--- ggmath.py
+++ ggmath.py
@@ -616,8 +616,9 @@ class Circle(_MathVisual):
         #ilist is a list of boundary intercepts that are screen-relative
         #print("ilist: ", ilist)
         if len(xcepts) > 1:
+            print("xcepts ", xcepts)
             ilist = [xcepts[0], xcepts[-1]]
-            print(ilist)
+            print("ilist ", ilist)
             xrange = ilist[-1][0] - ilist[0][0]
             yrange = ilist[-1][1] - ilist[0][1]
             numpoints = 20
