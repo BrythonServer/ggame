1db98b51d3162b4acd5d9c46934bb3adf72c9a06
Eric Dennison <ericd@netdenizen.com>
2018-02-23 20:08:10 -0500

    Updated from Brython Server: 2/23/2018 8:08:09 PM

--- ggrocket.py
+++ ggrocket.py
@@ -322,9 +322,7 @@ class Planet(MathApp):
 if __name__ == "__main__":
     
     
-    #Planet(Rocket, scale=0.0001, timezoom=2.2, altitude=804672, direction=0, velocity=8000)  # 500 miles, orbital velocity
-    Planet(Rocket)  # 500 miles, orbital velocity
-    #Planet(Rocket)
+    Planet(Rocket, scale=0.0001, timezoom=2.2, altitude=804672, direction=0, velocity=8000)  # 500 miles, orbital velocity
 
 
 
