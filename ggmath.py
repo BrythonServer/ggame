4c7ff817b90a0aa1c7ee753d87b2a8f1bafac5de
Eric Dennison <ericd@netdenizen.com>
2018-03-21 11:02:37 -0400

    Updated from Brython Server: 3/21/2018 11:02:36 AM

--- ggmath.py
+++ ggmath.py
@@ -312,7 +312,8 @@ class _MathVisual2(Sprite, _MathDynamic, metaclass=ABCMeta):
 class Label2(_MathVisual2):
     
     def __init__(self, *args, **kwargs):
-        super().__init__(self._buildAsset(), ['pos'], ['text'], *args, **kwargs)
+        super().__init__(TextAsset(""), ['pos'], ['text'], *args, **kwargs)
+        self._touchAsset()
 
     def _buildAsset(self):
         return TextAsset(self.nposinputs.text(), 
