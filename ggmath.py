056cd7919fdf2c9ec13b33cdfb2e5d6c05bcd4ee
Eric Dennison <ericd@netdenizen.com>
2018-03-21 11:37:49 -0400

    Updated from Brython Server: 3/21/2018 11:37:48 AM

--- ggmath.py
+++ ggmath.py
@@ -1432,8 +1432,6 @@ if __name__ == "__main__":
 
     label = Label2((100,150), lambda : "{0}".format(vslider.value), size=15, positioning="physical")
     label.movable = True
-    print(vslider())
-    print(label())
 
    
     def zoomCheck(**kwargs):
