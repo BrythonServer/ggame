1c4557872290e678ed808e9c6b64e54a9eb9d9e3
Eric Dennison <ericd@netdenizen.com>
2018-04-22 18:21:51 -0400

    Updated from Brython Server: 4/22/2018 6:21:50 PM

--- ggmath.py
+++ ggmath.py
@@ -357,6 +357,8 @@ class Slider2(_MathVisual2):
         self.selectable = True  # must be after super init!
         self.strokable = True  # this enables grabbing/slideing the thumb
         self.thumbcaptured = False
+        ###  this is returning zero at this point
+        print(self.sstdinputs.width())
         self._thumbwidth = max(self.sstdinputs.width()/40, 1)
         self.thumb = Sprite(RectangleAsset(self._thumbwidth, 
             self.sstdinputs.size()-2, LineStyle(1, self.stdinputs.color()), self.stdinputs.color()), 
