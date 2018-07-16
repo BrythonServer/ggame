24a5c8f56c44ee3859aa0a341257d7f21a7cce62
Eric Dennison <ericd@netdenizen.com>
2018-07-15 21:17:30 -0400

    Updated from Brython Server: 7/15/2018 9:17:29 PM

--- ggmath.py
+++ ggmath.py
@@ -1791,7 +1791,7 @@ if __name__ == "__main__":
     button = InputButton2(buttoncoords, "Press Me", pressbutton, size=15, positioning="physical")
     numinput = InputNumeric2((300, 275), 3.14, positioning="physical")
 
-    p1 = Point2((0,0))
+    p1 = Point2((0,0), color=Color(0x008000, 1))
     p1.movable = True
     
     p2 = Point2((0,-1))
