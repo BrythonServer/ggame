4c2ee108b805a924ab7478d15a65caff0b52c9e9
Eric Dennison <ericd@netdenizen.com>
2018-07-23 15:17:35 -0400

    Updated from Brython Server: 7/23/2018 3:17:34 PM

--- ggmath.py
+++ ggmath.py
@@ -251,7 +251,7 @@ class _MathVisual2(Sprite, _MathDynamic, metaclass=ABCMeta):
         self.GFX.visible = visible        
         if App._win != None:
             App._win.add(self.GFX)
-        self.position = self.pposinputs.pos
+        #self.position = self.pposinputs.pos
             
     @property
     def movable(self):
