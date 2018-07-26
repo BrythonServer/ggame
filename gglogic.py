77eb74f1dbbdf6fd3e7a0bce5ce383a1f4f13e72
Eric Dennison <ericd@netdenizen.com>
2018-07-26 16:51:42 -0400

    Updated from Brython Server: 7/26/2018 4:51:41 PM

--- gglogic.py
+++ gglogic.py
@@ -9,10 +9,7 @@ class BoolNOT(_MathDynamic):
     def __init__(self, *args, **kwargs):
         """
         Required Inputs
-        
-        * **name** identifying text name
         """
-        self.name = args[0]
         self._input = self.Eval(None)
         super().__init__()
 
@@ -38,10 +35,23 @@ class BoolAND(_MathDynamic):
     def __init__(self, *args, **kwargs):
         """
         Required Inputs
-        
-        * **name** identifying text name
         """
+        self._input = [self.Eval(None), self.Eval(None)]
+        super().__init__()
+
+    def __cal__(self):
+        for v in self._input:
+            if not v():
+                return False
+        return True
         
+    @property
+    def In(self):
+        return self._input
+    
+    @In.setter
+    def In(self, val)
+        self._input = [self.Eval(v) for v in list(val)]
 
 
 # test code here
@@ -50,7 +60,13 @@ if __name__ == "__main__":
     from ggmath import GlassButton, LEDIndicator
     
 
-    IC1 = BoolNOT("IC1")
+    IC1 = BoolNOT()
+    IC2 = BoolAND()
+    
+    b1 = GlassButton(None, (1,0))
+    b2 = GlassButton(None, (1,0.2))
+    d2 = LEDIndicator((1.5,0.1), IC2)
+    
     button = GlassButton(None, (0,0))
     LED = LEDIndicator((0,-1), IC1)
     IC1.In = button 
