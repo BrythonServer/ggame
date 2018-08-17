962a0cfb73b45e9cd9c921c75118292d164af40f
Eric Dennison <ericd@netdenizen.com>
2018-08-17 17:23:42 -0400

    Updated from Brython Server: 8/17/2018 5:23:42 PM

--- gglogic.py
+++ gglogic.py
@@ -305,6 +305,7 @@ if __name__ == "__main__":
     d2 = LEDIndicator((0.5,-0.5), IC1.Q_)
     """
 
+    """ JK Flip Flop demo
     IC1 = BoolJKFF()
 
     t1 = MetalToggle(0, (0,0.5))
@@ -317,7 +318,7 @@ if __name__ == "__main__":
 
     d1 = LEDIndicator((0.5,0.5), IC1)
     d2 = LEDIndicator((0.5,-0.5), IC1.Q_)
-
+    """
     
     app = MathApp()
     app.run()
