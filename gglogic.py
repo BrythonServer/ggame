a1e004d2eda291e5ac72e04c4df23b6719e8e8a5
Eric Dennison <ericd@netdenizen.com>
2018-08-17 16:15:06 -0400

    Updated from Brython Server: 8/17/2018 4:15:06 PM

--- gglogic.py
+++ gglogic.py
@@ -152,8 +152,8 @@ class BoolSRFF(_BoolOneInput):
         super().__init__(*args, **kwargs)
         self.IC1 = BoolNOR()
         self.IC2 = BoolNOR()
-        self.IC1.In = self.GetInput('R'), self.IC2
-        self.IC2.In = self.GetInput('S'), self.IC1
+        #self.IC1.In = self.GetInput('R'), self.IC2
+        #self.IC2.In = self.GetInput('S'), self.IC1
         
     def _getvalue(self):
         return self.IC1()
