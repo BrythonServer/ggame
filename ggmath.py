9db957d7413d5d8c374a9b89c3ae7e90ae65b77c
Eric Dennison <ericd@netdenizen.com>
2018-09-12 13:09:21 -0400

    Updated from Brython Server: 9/12/2018 1:09:21 PM

--- ggmath.py
+++ ggmath.py
@@ -1570,38 +1570,44 @@ if __name__ == "__main__":
     # c1 = Circle((0,-5000000), 5000000, LineStyle(1, Color(0x008040,1)), Color(0x008400,0.5))
     """
     
-#from ggmath import MathApp, Circle
-from math import  acos, pi, cos, sin
-
-
-# angle between circles n and n+1, shrink factor "a"
-th = lambda n, a: acos(((1+a**(n+1))**2+(1+a**n)**2-(a**(n+1)+a**n)**2)/(2*(1+a**(n+1))*(1+a**n)))
-
-# distance to center of nth circle, shrink factor "a", base circle radius r
-d = lambda n, a, r: r*(1 + a**(n+1))
-
-# total angle for center of nth circle (recursive)
-thtot = lambda n, a: 0 if n == 0 else th(n,a) + thtot(n-1,a)
-
-# position of center of nth circle, shrink factor a, base circle radius r
-def pos(n, a, r):
-    r = d(n, a, r)  # get distance to center
-    angle = thtot(n,a)  # angle to center
-    return (r*cos(angle), r*sin(angle))
-
-circleqty = 5
-a = 0.8
-r = 2
-angle = 0
-
-# draw the base circle
-Circle((0,0), r)
-
-# draw the children
-for n in range(circleqty):
-    center = pos(n, a, r)
-    Circle(center, r*a**(n+1))
-
+    #from ggmath import MathApp, Circle
+    from math import  acos, pi, cos, sin
+    
+    
+    # angle between circles n and n+1, shrink factor "a"
+    th = lambda n, a: acos(((1+a**(n+1))**2+(1+a**n)**2-(a**(n+1)+a**n)**2)/(2*(1+a**(n+1))*(1+a**n)))
+    
+    #angle between circle n and 1 (last to first), shrink factor "a"
+    thlast = lambda n, a: acos(((1+a)**2+(1+a**n)**2-(a+a**n)**2)/(2*(1+a)*(1+a**n)))
+    
+    # distance to center of nth circle, shrink factor "a", base circle radius r
+    d = lambda n, a, r: r*(1 + a**(n+1))
+    
+    # total angle for center of nth circle (recursive)
+    thtot = lambda n, a: 0 if n == 1 else th(n,a) + thtot(n-1,a)
+    
+    # position of center of nth circle, shrink factor a, base circle radius r
+    def pos(n, a, r):
+        r = d(n, a, r)  # get distance to center
+        angle = thtot(n,a)  # angle to center
+        return (r*cos(angle), r*sin(angle))
+    
+    circleqty = 7
+    a = 0.9485
+    r = 0.5
+    angle = 0
+    
+    # draw the base circle
+    Circle((0,0), r)
+    
+    # draw the children
+    for n in range(1, circleqty+1):
+        center = pos(n, a, r)
+        Circle(center, r*a**(n+1))
+        
+    print(thtot(circleqty, a) + thlast(circleqty, a))
+    
+    
 
 
     ap = MathApp()
