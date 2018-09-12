1d53ececd9d6aabdded1974d0fc12618788e381b
Eric Dennison <ericd@netdenizen.com>
2018-09-12 12:57:03 -0400

    Updated from Brython Server: 9/12/2018 12:57:03 PM

--- ggmath.py
+++ ggmath.py
@@ -1570,7 +1570,7 @@ if __name__ == "__main__":
     # c1 = Circle((0,-5000000), 5000000, LineStyle(1, Color(0x008040,1)), Color(0x008400,0.5))
     """
     
-    from ggmath import MathApp, Circle
+#from ggmath import MathApp, Circle
 from math import  acos, pi, cos, sin
 
 
@@ -1581,7 +1581,7 @@ th = lambda n, a: acos(((1+a**(n+1))**2+(1+a**n)**2-(a**(n+1)+a**n)**2)/(2*(1+a*
 d = lambda n, a, r: r*(1 + a**(n+1))
 
 # total angle for center of nth circle (recursive)
-thtot = lambda n, a: 0 if n == 0 else th(n,a) + th(n-1,a)
+thtot = lambda n, a: 0 if n == 0 else th(n,a) + thtot(n-1,a)
 
 # position of center of nth circle, shrink factor a, base circle radius r
 def pos(n, a, r):
