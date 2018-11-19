00d47457863a19bc04949b76cca348d76486cad6
Eric Dennison <ericd@netdenizen.com>
2018-11-19 00:58:50 +0000

    remove old doc build

--- compile.sh
+++
@@ -1,7 +0,0 @@
-export PYTHONPATH=./
-pdoc ggame.py --html --html-dir out --overwrite
-pdoc ggmath.py --html --html-dir out --overwrite
-pdoc ggrocket.py --html --html-dir out --overwrite
-mv out/ggame.m.html out/index.html
-mv out/ggmath.m.html out/ggmath.html
-mv out/ggrocket.m.html out/ggrocket.html
