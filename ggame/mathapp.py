5b439dcc82ec7e58e803d10c4fbb0815ad7e0aaf
Eric Dennison <ericd@netdenizen.com>
2018-11-29 18:01:23 +0000

    mathapp typo

--- ggame/mathapp.py
+++ ggame/mathapp.py
@@ -368,7 +368,7 @@ class MathApp(App):
         This will clean up any class level storage.
         """ 
         App._destroy(*args)  # hit the App class first
-        MathAp.time = None
+        MathApp.time = None
         MathApp._mathVisualList = [] 
         MathApp._mathDynamicList = []
         MathApp._mathMovableList = []
