a9928a182b87b9faedf956293a617fe24794d3a1
Eric Dennison <ericd@netdenizen.com>
2018-11-29 18:04:59 +0000

    debugging mathapp timer

--- ggame/mathapp.py
+++ ggame/mathapp.py
@@ -367,6 +367,7 @@ class MathApp(App):
         """
         This will clean up any class level storage.
         """ 
+        print("DEBUG: clearing mathapp class members!")
         App._destroy(*args)  # hit the App class first
         MathApp.time = None
         MathApp._mathVisualList = [] 
