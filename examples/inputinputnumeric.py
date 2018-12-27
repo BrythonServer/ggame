8a3026b9636fddd35ac354499ea7bf41d97618c5
Eric Dennison <ericd@netdenizen.com>
2018-12-27 03:30:02 +0000

    clean up input.py

---
+++ examples/inputinputnumeric.py
@@ -0,0 +1,9 @@
+from ggame.input import InputNumeric
+from ggame.mathapp import MathApp
+
+p = InputNumeric(
+    (300, 275),     # screen coordinates of input
+    3.14,           # initial value
+    positioning="physical") # use physical coordinates
+
+MathApp().run()
