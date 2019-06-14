d3555c3fd856c18def9e180bf0958ccd82f3b821
ericd <ericd@netdenizen.com>
2019-06-13 21:44:40 -0400

    debug

--- ggame/app.py
+++ ggame/app.py
@@ -175,8 +175,8 @@ class App:
             #App.win.unbind(MouseEvent.dblclick, self._mouseEvent)
             #App.win.destroy()
         #App.win = None
-        #for s in list(App.spritelist):
-        #    s.destroy()
+        for s in list(App.spritelist):
+            s.destroy()
         #App.spritelist = []
         #App._spritesdict = {}
         #App._eventdict = {}
