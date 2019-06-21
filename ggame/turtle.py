f47b4d41ef1f1c82789cfa1d07a7443abe1b3b21
ericd <ericd@netdenizen.com>
2019-06-21 15:57:59 -0400

    turtle testing

--- ggame/turtle.py
+++ ggame/turtle.py
@@ -1,7 +1,10 @@
 from browser import document
 from turtle import *
 
-set_defaults(turtle_canvas_id = document['ggame-canvas'])
+set_defaults(
+    turtle_canvas_id = document['ggame-canvas'],
+    turtle_canvas_container = document['graphics-column']
+)
 
 
 
